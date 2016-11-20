04e9b418d9cf46bba804e2a01f6f0bdc9ddba051
Rebecca Yang <rebeccayang123@gmail.com>
2016-11-20 13:47:40 -0800

    Adds task model

---
+++ db/migrate/20161120214548_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.date :completed
+
+      t.timestamps null: false
+    end
+  end
+end
