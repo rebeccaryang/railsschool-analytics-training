2b9b26441d487f004e35677a60ae7488ba76a918
Rebecca Yang <rebeccayang123@gmail.com>
2016-11-22 11:16:20 -0800

    Reroutes registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  devise_for :users, controllers: {
+    registrations: 'registrations'
+  }
+  root 'pages#home'
+  resources :tasks,  except:  [:index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
